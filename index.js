84fa59a806154a4d06360a23b69d24a3405b9544
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-05-29 17:30:08 -0600

    updates

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { title } = require('process');
+
 // let menuAnswers = "";
 let addOrContinueGlobal = "";
+let menuAnswersGlobal = "";
+let empType = "";
 
-//style
-const headerElem = document.querySelector("#topheader");
-const cardTop = document.querySelector("#card-top");
-
-headerElem.style.backgroundColor = "red";
-headerElem.style.backgroundColor = "green";
-cardTop.style.border = "5px solid red";
 
 inquirer.prompt([
     {
@@ -20,13 +16,18 @@ inquirer.prompt([
     },
     {
         type: 'input',
-        name: 'empMgrId',
+        name: 'mgrId',
         message: 'What is their employee ID?'
     },
     {
         type: 'input',
-        name: 'EmpMgrEmail',
+        name: 'mgrEmail',
         message: 'What is their email address?'
+    },
+    {
+        type: 'input',
+        name: 'mgrOfficeNum',
+        message: 'What is their office number?'
     },
     {
         type: 'list',
@@ -38,25 +39,30 @@ inquirer.prompt([
 ]).then(menuAnswers => {
     // menuAnswers = menuAnswers;
     addOrContinueGlobal = menuAnswers.addOrContinue;
+    menuAnswersGlobal = menuAnswers;
     checkForAnswer();
 })
-
 function engrQuestions() {
     inquirer.prompt([
         {
             type: 'input',
-            name: 'engrsName',
+            name: 'engrName',
             message: 'What is the engineers name?'
         },
         {
             type: 'input',
-            name: 'empMgrId',
-            message: "What is the engineer's, manager's ID?"
+            name: 'engrrId',
+            message: "What is the ?"
         },
         {
             type: 'input',
-            name: 'engrsEmail',
+            name: 'engrEmail',
             message: 'What is their email address?'
+        },
+        {
+            type: 'input',
+            name: 'engrGh',
+            message: 'What is their GitHub username?'
         },
 
     ]).then(engrAnswers => {
@@ -94,8 +100,8 @@ function internQuestions() {
             name: 'internsEmail',
             message: 'What is their email address?'
         },
-
     ]).then(internAnswers => {
+        empType = "Intern";
         console.log(internAnswers);
         inquirer.prompt([
             {
@@ -115,19 +121,17 @@ function internQuestions() {
     })
 };
 
-
-
 function buildHtml() {
-    console.log("build html");
+
     fs.writeFileSync('index.html', `
-    
-            <!DOCTYPE html>
+    <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
 
@@ -138,19 +142,57 @@ function buildHtml() {
     <section>
         <div id="card-top">
             <div id="card-container">
-                <div id="cards">
-                    <div>
-
+                <div id="card1">
+                    <div id="cardheader1">
+                        <h2>${menuAnswersGlobal.mgrName}</h2>
+                        <h3>Manager<h3>
                     </div>
-                    <div>
-
+                    <div id="cardcontent1">
+                        <div id="cardlist1">
+                            <p id="contentitem1">ID: ${menuAnswersGlobal.mgrId}</p>
+                            <p id="contentitem2">Email: ${menuAnswersGlobal.mgrEmail}</p>
+                            <p id="contentitem3">Office number: ${menuAnswersGlobal.mgrOfficeNum}</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="card2">
+                    <div id="cardheader2">
+                        <h2></h2>
+                    </div>
+                    <div id="cardcontent2">
+                        <div id="cardlist2">
+                            <p id="contentitem1">card content</p>
+                            <p id="contentitem2">card content</p>
+                            <p id="contentitem3">card content</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="card3">
+                    <div id="cardheader3">
+                        <h2></h2>
+                    </div>
+                    <div id="cardcontent3">
+                        <div id="cardlist3">
+                            <p id="contentitem1">card content</p>
+                            <p id="contentitem2">card content</p>
+                            <p id="contentitem3">card content</p>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
         <div id="card-bottom">
-
+            <div id="card-container">
+                <div id="card4">
+                    <h2>four</h2>
+                    <p>card content</p>
+                </div>
+                <div id="card5">
+                    <h2>five</h2>
+                    <p>card content</p>
+                </div>
+            </div>
+        </div>
         </div>
     </section>
     <script src="./index.js"></script>
@@ -160,11 +202,6 @@ function buildHtml() {
     `)
 }
 
-
-
-
-
-
 function checkForAnswer() {
     if (addOrContinueGlobal === "Engineer") {
         engrQuestions();
@@ -172,6 +209,8 @@ function checkForAnswer() {
         internQuestions();
     } else {
         buildHtml();
+        console.log("Success!");
+        console.log(menuAnswersGlobal.mgrEmail);
     }
 
 }
